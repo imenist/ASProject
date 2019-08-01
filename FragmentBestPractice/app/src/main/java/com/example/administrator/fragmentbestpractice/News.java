644b38d212fb505419360c1bcabd80a6cc5ccee9
imenist <243260197@qq.com>
2019-08-01 20:38:12 +0800

    FragmentBest-8.1

---
+++ FragmentBestPractice/app/src/main/java/com/example/administrator/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.example.administrator.fragmentbestpractice;
+
+/**
+ * Created by Administrator on 2019/8/1.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
